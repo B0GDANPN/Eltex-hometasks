6b40553799a7414b14e0a94ab7ff02f8f33a1a2e
bogdan <b.pyanzin@mail.ru>
2024-04-26 23:31:26 +0700

    fix seven

---
+++ Part2/Seven/div.c
@@ -0,0 +1,4 @@
+int div(int a, int b) {
+    int res = a / b;
+    return res;
+}
---
+++ Part2/Seven/div.h
@@ -0,0 +1,8 @@
+//
+// Created by bogdan on 16.04.24.
+//
+
+#ifndef ELTEX_HOMETASKS_DIV_H
+#define ELTEX_HOMETASKS_DIV_H
+int div(int a, int b);
+#endif //ELTEX_HOMETASKS_DIV_H
